3e82530df3c643fc5b6bd1a40164ca76bc0505de
mbannert <bunny@hey.com>
2021-08-13 22:29:51 +0200

    hexagon swiss cross, col layout added

--- frontend/src/layouts/standard.vue
+++ frontend/src/layouts/standard.vue
@@ -3,7 +3,13 @@
 
     <q-header class="bg-primary text-white">
       <q-toolbar>
-
+        <svg width="25" height="25" viewBox="0 0 25 25">
+          <polygon points="12.5 0, 25 6.25, 25 18.75, 12.5 25.5, 0 18.75, 0 6.25" 
+      		 fill="red" stroke-width="5" />
+           <rect x="10" y="5" width="5" height="15" style="fill:#fff;" />
+           <rect x="5" y="10" width="15" height="5" style="fill:#fff;" />
+        </svg>
+    <div class="title-text">Labor Market Monitor</div>
       </q-toolbar>
     </q-header>
 
@@ -11,10 +17,16 @@
       <router-view />
     </q-page-container>
 
-    <q-footer class="bg-grey-8 text-white">
+    <q-footer>
       <q-toolbar>
+        <div class="footer-text">
+        This and that cause this and that was missing.
+        </div>
       </q-toolbar>
+
     </q-footer>
 
   </q-layout>
 </template>
+
+
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,6 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-      Mooo {{ $t('common.cow') }}
+  <q-page>
+
+    <div class="lmm-page">
+      <div class="row">
+        <div class="q-card col-12">
+          <div class="row">
+          <div class="col-4">Map</div>
+          <div class="col-8">Time Series</div>
+          </div>      
+        </div>
+      </div>
+    </div>
+        
+   
+      
   </q-page>
 </template>
 
