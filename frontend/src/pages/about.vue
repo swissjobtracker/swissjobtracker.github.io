d391be4e9fac85bbb862c1bdb1436c5f4c19ab6d
mbannert <bunny@hey.com>
2021-09-01 11:37:33 +0200

    about page /w i18n

--- frontend/src/pages/about.vue
+++ frontend/src/pages/about.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="lmm-page">
-        <h1>About</h1>
+        <h3> {{ $t('about.title') }}</h3>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h4> {{ $t('about.project_title') }}</h4>
+                <p>
+                {{ $t('about.project_text') }}
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h4> {{ $t('about.partner_title') }}</h4>
+                <p>
+                    {{ $t('about.project_text') }}
+                </p>
+            </div>
+        </div>
     </div>
 </template>
