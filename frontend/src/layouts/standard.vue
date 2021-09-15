46dd7e52d03e0b2fe1b129d87054c329e8694800
mbannert <bunny@hey.com>
2021-09-15 23:10:57 +0200

    added snf / nrp logos

--- frontend/src/layouts/standard.vue
+++ frontend/src/layouts/standard.vue
@@ -3,13 +3,7 @@
 
     <q-header class="bg-primary text-white">
       <q-toolbar class="lmm-bar">
-        <a class="title-link" href="/"><svg width="25" height="25" viewBox="0 0 25 25">
-          <polygon points="12.5 0, 25 6.25, 25 18.75, 12.5 25.5, 0 18.75, 0 6.25" 
-      		 fill="red" stroke-width="5" />
-           <rect x="10" y="5" width="5" height="15" style="fill:#fff;" />
-           <rect x="5" y="10" width="15" height="5" style="fill:#fff;" />
-        </svg></a>
-    <div class="title-text">Labor Market Monitor</div>
+    <div class="title-text">.. SWISS Job Tracker .. </div>
       <q-tabs>
         <q-route-tab  v-for="(menuItem) in menuList" :to="menuItem.to" :label="menuItem.label" :key="menuItem.to" />
       </q-tabs>
@@ -23,10 +17,12 @@
 
     <q-footer>
      <div class="row q-col-gutter-lg logo-row">
-      <div class="col-12 col-md-3"><a href="https://kof.ethz.ch"><img src="kof-neg.png" height=25></a></div>
-      <div class="col-12 col-md-3"><a href="https://ethz.ch"><img src="eth-neg.png" height=25></a></div>
-      <div class="col-12 col-md-3"><a href="https://unil.ch"><img src="unil-neg.png" height=25></a></div>
-      <div class="col-12 col-md-3"><a href="https://"><img src="x28-neg.png" height=25></a></div>
+      <div class="col-12 col-md-2"><a href="https://kof.ethz.ch"><img src="kof-neg.png" height=25></a></div>
+      <div class="col-12 col-md-2"><a href="https://ethz.ch"><img src="eth-neg.png" height=25></a></div>
+      <div class="col-12 col-md-2"><a href="https://"><img src="nfp_en.png" height=25></a></div>
+      <div class="col-12 col-md-2"><a href="https://"><img src="snf_en.svg" height=25></a></div>
+      <div class="col-12 col-md-2"><a href="https://unil.ch"><img src="unil-neg.png" height=25></a></div>
+      <div class="col-12 col-md-2"><a href="https://"><img src="x28-neg.png" height=25></a></div>
       </div>
       <hr>
       <div class="row q-col-gutter-lg">
