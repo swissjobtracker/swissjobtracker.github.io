fdb94b6b00df1fa7ffff8f795f120d2bcd9cb160
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2022-09-28 14:31:38 +0200

    finalize code popup

--- src/components/CodePopup.vue
+++ src/components/CodePopup.vue
@@ -9,9 +9,12 @@
       <div v-html="createSnippet(series)" id="code_snippet"></div>
     </q-card-section>
     <q-card-actions align="right">
-      <q-btn dense flat @click="handleCopy" icon="content_copy">
-          <q-tooltip class="bg-white text-primary">Copy R Code to clipboard</q-tooltip>
-        </q-btn>
+      <q-btn v-if="!copyPossible && selectionPossible" dense flat @click="handleSelectAll" icon="select_all">
+        <q-tooltip class="bg-white text-primary">{{$t("copy_code.select_button")}}</q-tooltip>
+      </q-btn>
+      <q-btn v-if="copyPossible" dense flat @click="handleCopy" icon="content_copy">
+          <q-tooltip class="bg-white text-primary">{{$t("copy_code.copy_button")}}</q-tooltip>
+      </q-btn>
     </q-card-actions>
   </q-card>
 </template>
@@ -42,8 +45,31 @@ export default {
           })
         })
       },
+      handleSelectAll: function(e) {
+        e.preventDefault()
+        if(window.getSelection) {
+          window.getSelection().selectAllChildren(
+            document.getElementById("code_snippet")
+          )
+        } else {
+          this.$q.notify({
+            message: this.$t("copy_code.select_error"),
+            color: 'negative',
+            position: 'top',
+            timeout: 2000
+          })
+        }
+      },
       createSnippet,
       copySnippet
+  },
+  computed: {
+    selectionPossible() {
+      return window.getSelection !== undefined
+    },
+    copyPossible() {
+      return navigator && navigator.clipboard && navigator.clipboard.writeText && true
+    }
   }
 }
 </script>
