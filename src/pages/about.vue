606f59704bb3329ede7d22d18ce7caf9ed98e9ee
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2022-10-25 12:16:15 +0200

    tweak about page, add symbolbild

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,21 +1,60 @@
 <template>
     <div class="lmm-page">
-        <h3> {{ $t('about.title') }}</h3>
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <h4> {{ $t('about.project_title') }}</h4>
-                <p>
-                {{ $t('about.project_text') }}
-                </p>
+      <q-card class="about-card">
+        <img src="about.jpg"/>
+
+        <q-card-section>
+          <div class="text-h3">
+            {{ $t('about.title') }}
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <q-card-section>
+            <div class="text-h4">
+              {{ $t('about.project_title') }}
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <h4> {{ $t('about.partner_title') }}</h4>
-                <p>
-                    {{ $t('about.partner_text') }}
-                </p>
+          </q-card-section>
+          <q-card-section>
+            <div>
+              {{ $t('about.project_text') }}
             </div>
-        </div>
+          </q-card-section>
+        </q-card-section>
+
+        <q-card-section>
+          <q-card-section>
+            <div class="text-h4">
+              {{ $t('about.methodology_title') }}
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <div>
+              {{ $t('about.methodology_text') }}
+            </div>
+          </q-card-section>
+        </q-card-section>
+
+        <q-card-section>
+          <q-card-section>
+            <div class="text-h4">
+              {{ $t('about.partner_title') }}
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <div>
+              {{ $t('about.partner_text') }}
+            </div>
+          </q-card-section>
+        </q-card-section>
+      </q-card>
     </div>
 </template>
+
+<style scoped>
+.about-card {
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
