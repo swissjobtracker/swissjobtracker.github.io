024b839863663e5ac0fdd81404267ef1c87cece5
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2022-10-31 15:36:21 +0100

    reorganize card sections

--- src/components/ComparisonCard.vue
+++ src/components/ComparisonCard.vue
@@ -1,32 +1,33 @@
 <template>
      <div class="row">
         <q-card square bordered class="shadow-1 q-card q-mb-md col-12">
-          <div class="row q-col-gutter-xl">
-            <div class="col-12 col-md-5">
-              <series-selector @select="onSelect" @changeMode="(newMode) => selectedMode = newMode" :colors="colors" :activeDate="activeDate"/>
+          <q-card-actions align="right" class="q-pa-none card-action-bar">
+                <q-btn flat @click="show_code = true">
+                  <q-tooltip>{{$t("card.btn_tooltips.code_snippet")}}</q-tooltip>
+                  <q-icon name="terminal"></q-icon>
+                </q-btn>
+                <q-btn flat @click="$refs.linechart.downloadChart()">
+                  <q-icon name="ssid_chart"></q-icon>
+                  <q-tooltip>{{$t("card.btn_tooltips.download_chart")}}</q-tooltip>
+                </q-btn>
+                <q-btn @click="closeCard" flat>
+                <q-tooltip>{{$t("card.btn_tooltips.close")}}</q-tooltip>
+                  <q-icon name="close"></q-icon>
+                </q-btn>
+            </q-card-actions>
+            <q-card-sect>
+              <div class="row q-col-gutter-xl">
+                <div class="col-12 col-md-5">
+                  <series-selector @select="onSelect" @changeMode="(newMode) => selectedMode = newMode" :colors="colors" :activeDate="activeDate"/>
+                </div>
+                <div class="col-12 col-md-7">
+                  <linechart :series="selectedSeries" :mode="selectedMode" @setActiveDate="onSetActiveDate" :colors="colors" ref="linechart"/>
+                <q-dialog v-model="show_code">
+                  <code-popup :series="selectedSeries" />
+                </q-dialog>
+              </div>
             </div>
-            <div class="col-12 col-md-7">
-              <q-card-actions align="right" class="q-pa-none">
-                    <q-btn flat @click="show_code = true">
-                      <q-tooltip>{{$t("card.btn_tooltips.code_snippet")}}</q-tooltip>
-                      <q-icon name="terminal"></q-icon>
-                    </q-btn>
-                    <q-btn flat @click="$refs.linechart.downloadChart()">
-                      <q-icon name="ssid_chart"></q-icon>
-                      <q-tooltip>{{$t("card.btn_tooltips.download_chart")}}</q-tooltip>
-                    </q-btn>
-                    <q-btn @click="closeCard" flat>
-                    <q-tooltip>{{$t("card.btn_tooltips.close")}}</q-tooltip>
-                      <q-icon name="close"></q-icon>
-                    </q-btn>
-                </q-card-actions>
-              <linechart :series="selectedSeries" :mode="selectedMode" @setActiveDate="onSetActiveDate" :colors="colors" ref="linechart"/>
-            <q-dialog v-model="show_code">
-              <code-popup :series="selectedSeries" />
-            </q-dialog>
-          </div>
-
-          </div>
+          </q-card-sect>
         </q-card>
       </div>
 </template>
@@ -68,3 +69,7 @@ export default {
 
 </script>
 
+<style lang="sass" scoped>
+.card-action-bar
+  background-color: scale-color($secondary, $lightness: 60%)
+</style>
