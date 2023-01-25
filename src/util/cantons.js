de55748113b82890d006495b39b0c4f0b362f02f
Stéphane Bisinger <bisinger@kof.ethz.ch>
2023-01-25 13:07:31 +0100

    Correct canton names

--- src/util/cantons.js
+++ src/util/cantons.js
@@ -1,112 +1,114 @@
 export const cantons = [
   {
-    label: 'Aargau',
-    value: 'ag',
+    label: "Aargau",
+    value: "ag",
   },
   {
-    label: 'Appenzell Innerrhoden',
-    value: 'ai',
+    label: "Appenzell Innerrhoden",
+    value: "ai",
   },
   {
-    label: 'Appenzell Ausserrhoden',
-    value: 'ar',
+    label: "Appenzell Ausserrhoden",
+    value: "ar",
   },
   {
-    label: 'Bern',
-    value: 'be',
+    label: "Bern",
+    value: "be",
   },
   {
-    label: 'Basel-Landschaft',
-    value: 'bl',
+    label: "Basel-Landschaft",
+    value: "bl",
   },
   {
-    label: 'Basel-Stadt',
-    value: 'bs',
+    label: "Basel-Stadt",
+    value: "bs",
   },
   {
-    label: 'Fribourg',
-    value: 'fr',
+    label: "Fribourg",
+    value: "fr",
   },
   {
-    label: 'Genève',
-    value: 'ge',
+    label: "Genève",
+    value: "ge",
   },
   {
-    label: 'Glarus',
-    value: 'gl',
+    label: "Glarus",
+    value: "gl",
   },
   {
-    label: 'Grishun',
-    value: 'gr',
+    label: "Graubünden",
+    value: "gr",
   },
   {
-    label: 'Jura',
-    value: 'ju',
+    label: "Jura",
+    value: "ju",
   },
   {
-    label: 'Luzern',
-    value: 'lu',
+    label: "Luzern",
+    value: "lu",
   },
   {
-    label: 'Neuchâtel',
-    value: 'ne',
+    label: "Neuchâtel",
+    value: "ne",
   },
   {
-    label: 'Nidwalden',
-    value: 'nw',
+    label: "Nidwalden",
+    value: "nw",
   },
   {
-    label: 'Obwalden',
-    value: 'ow',
+    label: "Obwalden",
+    value: "ow",
   },
   {
-    label: 'St. Gallen',
-    value: 'sg',
+    label: "St. Gallen",
+    value: "sg",
   },
   {
-    label: 'Schaffhausen',
-    value: 'sh',
+    label: "Schaffhausen",
+    value: "sh",
   },
   {
-    label: 'Solothurn',
-    value: 'so',
+    label: "Solothurn",
+    value: "so",
   },
   {
-    label: 'Schwyz',
-    value: 'sz',
+    label: "Schwyz",
+    value: "sz",
   },
   {
-    label: 'Thurgau',
-    value: 'tg',
+    label: "Thurgau",
+    value: "tg",
   },
   {
-    label: 'Ticino',
-    value: 'ti',
+    label: "Ticino",
+    value: "ti",
   },
   {
-    label: 'Uri',
-    value: 'ur',
+    label: "Uri",
+    value: "ur",
   },
   {
-    label: 'Vaud',
-    value: 'vd',
+    label: "Vaud",
+    value: "vd",
   },
   {
-    label: 'Vallais',
-    value: 'vs',
+    label: "Valais",
+    value: "vs",
   },
   {
-    label: 'Zug',
-    value: 'zg',
+    label: "Zug",
+    value: "zg",
   },
   {
-    label: 'Zürich',
-    value: 'zh'
-  }
-].sort((a, b) => a.value > b.value ? 1 : -1)
+    label: "Zürich",
+    value: "zh",
+  },
+].sort((a, b) => (a.value > b.value ? 1 : -1));
 
 export const getCantonByCode = (code) => {
-  return cantons.find(({value}) => value == code.toLowerCase())?.label || undefined
-}
+  return (
+    cantons.find(({ value }) => value == code.toLowerCase())?.label || undefined
+  );
+};
 
-export default cantons
+export default cantons;
