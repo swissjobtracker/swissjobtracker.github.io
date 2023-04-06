fc7a33f8792365bf026bde69cc8baae62a32e503
Stéphane Bisinger <bisinger@kof.ethz.ch>
2023-04-06 13:57:48 +0200

    Add note about the newest values in the indicator

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,14 +3,17 @@
 
     <div class="lmm-page">
 
-      <comp-card
-        v-for="(card) in cards"
-        :key="card.id"
-        @close="() => removeCard(card.id)"
-       />
+      <div :class="$style['info-box']" class="text-body-1 q-pa-md q-ma-sm">
+        <h6 class="q-ma-sm">NOTE</h6>
+        <p>
+          The newest values of the indicator are often somewhat too low and revised upward in subsequent weeks. We
+          will soon publish a revised indicator without this feature.
+        </p>
+      </div>
+      <comp-card v-for="(card) in cards" :key="card.id" @close="() => removeCard(card.id)" />
 
-     <q-btn  color="secondary" class="full-width add_chart" @click="newCard">
-       {{ $t('common.add_chart')}}
+      <q-btn color="secondary" class="full-width add_chart" @click="newCard">
+        {{ $t('common.add_chart') }}
       </q-btn>
 
 
@@ -36,21 +39,32 @@ export default {
 
   },
   data() {
-    return{
+    return {
       cards: [
-        {id: uuidv4()}
+        { id: uuidv4() }
       ],
     }
   },
   methods: {
-    newCard: function(){
+    newCard: function () {
       this.cards.push({
         id: uuidv4()
       })
     },
-    removeCard: function(id){
-      this.cards = this.cards.filter((c) => c.id != id )
+    removeCard: function (id) {
+      this.cards = this.cards.filter((c) => c.id != id)
     }
   }
 }
 </script>
+
+<style lang="scss" module>
+.info-box {
+  border: 3px solid $info;
+  background: white;
+
+  h6 {
+    color: $info;
+  }
+}
+</style>
