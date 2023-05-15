e9f6471ae23e8276946a38baa15f4e4c85d8a74d
Stéphane Bisinger <bisinger@kof.ethz.ch>
2023-05-15 16:01:26 +0200

    Upgrade vue-i18n

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,32 +1,24 @@
-import Vue from 'vue'
-import { createI18n } from 'vue-i18n'
+import { boot } from "quasar/wrappers";
+import { createI18n } from "vue-i18n";
 
-import messages from 'src/i18n'
+import messages from "src/i18n";
 
-//Vue.use(VueI18n)
-
-const lang = 'en'
+const lang = "en";
 
 const locales = {
-  de: 'de-ch',
-  fr: 'fr-ch',
-  it: 'it-ch',
-  en: 'en-us'
-}
-
-
+  de: "de-ch",
+  fr: "fr-ch",
+  it: "it-ch",
+  en: "en-us",
+};
 
 const i18n = createI18n({
   locale: locales[lang],
-  fallbackLocale: 'en-us',
-  messages
-})
+  fallbackLocale: "en-us",
+  messages,
+});
 
-export default ({ app }) => {
+export default boot(({ app }) => {
   // Set i18n instance on app
-  app.use(i18n)
-}
-
-// if you need to import it from
-// other files, then:
-export { i18n }
+  app.use(i18n);
+});
